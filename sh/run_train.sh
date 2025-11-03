68b7ca5676d12d8f7ffd4e971f6279ca7cc768e9
FillerSpeech <fillerspeech@gmail.com>
2025-11-03 12:00:18 +0900

    .

--- sh/run_train.sh
+++
@@ -1,4 +0,0 @@
-# # # # # # # # # # # # # # # # # # # # # Filler prediction Training # # # # # # # # # # # # # # # # # # # #
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 ./filler_prediction/train.py --cfg-path ./filler_prediction/configs/config_filler_pred.yaml
-
--- sh/run_tsboard.sh
+++
@@ -1,3 +0,0 @@
-log_dir='./filler_prediction/logs/filler_pred'
-
-tensorboard --logdir ${log_dir} --bind_all
